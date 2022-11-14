1f10001c3342eae2d1af078fb2f576881f5cfa7a
GokulBVMoback <gokulbv@moback.com>
2022-11-14 19:11:50 +0530

    Connection setup completed

---
+++ Entities/Models/TblUserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Models;
+
+public partial class TblUserRole
+{
+    public int UserRoleId { get; set; }
+
+    public string? UserRole { get; set; }
+
+    public virtual ICollection<TblUser> TblUsers { get; } = new List<TblUser>();
+}
