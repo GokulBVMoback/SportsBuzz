b8d20a8f1e655e57327de452d9e122e2fe41a726
MDKAZEEMUDDIN <mohammedk@moback.com>
2022-11-15 17:27:38 +0530

    I have completed user controller task

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using BAL.Abstraction;
+using BAL.Services;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.DbModels;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserService> _logger;
+        private readonly IUserInterface _userService;
+        private readonly DbSportsBuzzContext dbContext;
+
+
+        public UserController(DbSportsBuzzContext dbcontext, ILogger<UserService> logger, IUserInterface userService)
+        {
+            dbContext = dbcontext;
+            _logger = logger;
+            _userService = userService;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public JsonResult UserDetails()
+        {
+            try
+            {
+                return new JsonResult(_userService.GetUser().ToList());
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex);
+
+            }
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+         public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        [Route("registration")]
+
+        public JsonResult Registration(Registration User)
+        {
+            try
+            {
+                return new JsonResult(_userService.Registration(User));
+
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("LogIn")]
+
+        public JsonResult LogIn( LogIn logIn)
+        {
+            try
+            {
+                _userService.LogIn(logIn);
+                return new JsonResult(_userService.LogIn(logIn));
+
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+        // PUT api/<UserController>/5
+        [HttpPut("{Mail}")]
+        public JsonResult ForgetPassword(string Mail, ChangePassword changePassword)
+        {
+            try
+            {
+                changePassword.Email = Mail;
+                return new JsonResult(_userService.ForgetPassword(changePassword));
+
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+    }
+}
