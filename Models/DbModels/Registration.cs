aec5e2db7c6376462dbebe06c4869391b2fef91d
GokulBVMoback <gokulbv@moback.com>
2022-11-16 16:35:44 +0530

    changes in GetUser and registration and forgetpassword

--- Models/DbModels/Registration.cs
+++ Models/DbModels/Registration.cs
@@ -3,26 +3,7 @@ using System.Collections.Generic;
 
 namespace Entities.Models;
 
-public partial class Registration
+public class Registration:TblUser
 {
-    public int UserId { get; set; }
-
-    public string? FirstName { get; set; }
-
-    public string? LastName { get; set; }
-
-    public string? Email { get; set; }
-
-    public long? PhoneNum { get; set; }
-
-    public string? Password { get; set; }
     public string? ConfirmPassword { get; set; }
-
-    public int? UserRole { get; set; }
-
-    public DateTime? CreatedDate { get; set; }
-
-    public DateTime? UpdatedDate { get; set; }
-
-    public bool? Active { get; set; }
 }
---
+++ Models/DbModels/UserDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.DbModels;
+
+public class UserDisplay
+{
+    public int UserId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public long? PhoneNum { get; set; }
+
+    public string? UserRole { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public bool? Active { get; set; }
+
+}
