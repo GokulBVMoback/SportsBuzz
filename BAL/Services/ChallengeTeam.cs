1c99a2172568a32c1f6a3eb1569e6068de17a784
Dipteshpatra <113527751+Dipteshpatra@users.noreply.github.com>
2022-12-05 15:39:55 +0530

    Check avilable team completed

---
+++ BAL/Services/ChallengeTeam.cs
@@ -0,0 +1,38 @@
+﻿using BAL.Abstraction;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services
+{
+    public class ChallengeTeam
+    {
+        private readonly DbSportsBuzzContext _dbSportsBuzzContext;
+        private readonly ITeam _team;
+        public ChallengeTeam(DbSportsBuzzContext dbSportsBuzzContext, ITeam team)
+        {
+            _dbSportsBuzzContext = dbSportsBuzzContext;
+            _team = team;
+        }
+
+        public List<TeamList> ChechkAvilableTeam(SearchAvailableGround availablTeam)
+        {
+            List<TblChallenge> list = _dbSportsBuzzContext.TblChallenges.ToList().Where(x => x.SessionId == availablTeam.SessionId && x.Date == availablTeam.Date).ToList();
+            List<TeamList> Team = _team.SearchByCity(availablTeam.City!).ToList();
+            foreach (var items in list)
+            {
+                Team = Team.Where(x => x.TeamId != items.TeamId1 && x.TeamId != items.TeamId2).ToList();
+            }
+            return Team;
+        }
+        public bool ChallengingTeam()
+        {
+            return true;
+        }
+    }
+}
---
+++ Models/DbModels/SearchAvilableTeam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DbModels
+{
+    internal class SearchAvilableTeam
+    {
+    }
+}
